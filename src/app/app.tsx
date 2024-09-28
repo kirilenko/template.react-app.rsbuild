f62db8bb8ea7ec840f161a762403ea9bc705dd25
Kirilenko <n@kirilenko.com>
2024-09-29 00:14:45 +0400

    feat: add tailwind support to prettier-config

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -8,7 +8,7 @@ import './app.css'
 const App: FC = () => {
   return (
     <>
-      <div className="h-full flex items-center justify-center">
+      <div className="flex h-full items-center justify-center">
         <Title content="Rsbuild w/ React" />
       </div>
       <span className="hidden">{env.PUBLIC_TIMESTAMP}</span>
