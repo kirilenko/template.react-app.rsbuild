80d339c8f468ac1bb0cc529d3ab67f9fadbeeb66
Kirilenko <n@kirilenko.com>
2024-09-28 22:37:12 +0400

    feat: setup source-map

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from '@rsbuild/core'
+import { defineConfig, loadEnv, SourceMap } from '@rsbuild/core'
 import { pluginEslint } from '@rsbuild/plugin-eslint'
 import { pluginReact } from '@rsbuild/plugin-react'
 import path from 'path'
@@ -9,7 +9,17 @@ import path from 'path'
 // source: { define: publicVars }
 // For many public prefix vars use: loadEnv({ prefixes: ['REACT_APP_'] })
 
+const { parsed } = loadEnv()
+const SOURCEMAP_JS = (parsed.SOURCEMAP_JS || false) as SourceMap['js']
+
+console.log('● sourceMap.js:', SOURCEMAP_JS) // eslint-disable-line no-console
+
 export default defineConfig({
+  output: {
+    sourceMap: {
+      js: SOURCEMAP_JS,
+    },
+  },
   plugins: [pluginReact(), pluginEslint()],
   source: {
     alias: { '@': path.resolve(__dirname, './src') },
