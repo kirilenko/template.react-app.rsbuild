74fdeac42ecf1f602cc158d097c13fa101a2eb9e
Kirilenko <n@kirilenko.com>
2024-09-30 19:43:12 +0400

    feat: add additional common env-file

--- src/app/config/env/env.ts
+++ src/app/config/env/env.ts
@@ -14,6 +14,7 @@ const asString = (value: string | null = null): string => {
 }
 
 const env = {
+  PUBLIC_DEFAULT_PAGE_TITLE: asString(process.env.PUBLIC_DEFAULT_PAGE_TITLE),
   PUBLIC_SOME_BOOLEAN: asBoolean(process.env.PUBLIC_SOME_BOOLEAN),
   PUBLIC_SOME_NUMBER: asNumber(process.env.PUBLIC_SOME_NUMBER),
   PUBLIC_TIMESTAMP: asString(process.env.PUBLIC_TIMESTAMP),
